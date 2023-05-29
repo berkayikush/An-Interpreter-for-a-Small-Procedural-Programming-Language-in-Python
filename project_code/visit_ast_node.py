8be45e65f2ede56e1fa4121d18fd37c83986c200
Berkay Kush <berkay.i.kush@gmail.com>
2023-05-29 23:26:08 +0200

    Change the naming self.no_visit_method to self.no_visit

--- project_code/visit_ast_node.py
+++ project_code/visit_ast_node.py
@@ -1,6 +1,6 @@
 class ASTNodeVisitor:
     def visit(self, ast_node):
-        execute = getattr(self, "visit" + type(ast_node).__name__, self.no_visit_method)
+        execute = getattr(self, "visit" + type(ast_node).__name__, self.no_visit)
         return execute(ast_node)
 
     def no_visit(self, node):
