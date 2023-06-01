3b99b9bd4b88b47ef08281719a6f35bbadae2bd6
Berkay Kush <berkay.i.kush@gmail.com>
2023-06-01 11:02:35 +0200

    Change no_visit's parameter name from node to ast_node

--- project_code/visit_ast_node.py
+++ project_code/visit_ast_node.py
@@ -3,8 +3,8 @@ class ASTNodeVisitor:
         execute = getattr(self, "visit" + type(ast_node).__name__, self.no_visit)
         return execute(ast_node)
 
-    def no_visit(self, node):
+    def no_visit(self, ast_node):
         """
         Raise an exception if no visit method is implemented for a node.
         """
-        raise Exception(f"No visit_{type(node).__name__} method.")
+        raise Exception(f"No visit_{type(ast_node).__name__} method.")
